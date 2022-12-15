17459ae6ab70ff3aaeee1bf8a547ade090cccc37
Paulo Tintaya <pau.tintaya.code@gamil.com>
2022-12-15 17:29:38 -0400

    Add more countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+    let localtime = new Date();
+    let qatar = new Date(localtime.getTime() + (3600000 * 9));
+    const titulo = React.createElement(
+      'h1',
+      {},
+      'La hora es: '
+    );
+    root.render(titulo);
+    
+      const element = (
+        <div className="App">
+          <header className="App-header">
+            <h1>La hora es: </h1>
+            <h2>En Mexico: {new Date().toLocaleTimeString('es-MX')}</h2>
+            <h2>En Qatar: {qatar.toLocaleTimeString('es-MX')}</h2>
+
+
+          </header>
+        </div>
+
+      );
+    root.render(element);
+
+  
 }
 
 export default App;
